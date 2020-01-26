3d4fa8e82939088b53a8234abe66211a522eca1b
Jess Zhang <jesszhang24@gmail.com>
2020-01-26 01:15:02 -0500

    title, author, text, an error message if any of those is not found

--- app/request.py
+++ app/request.py
@@ -2,18 +2,6 @@ import requests
 import re
 import json
 
-url = 'https://www.nytimes.com/2020/01/25/opinion/sunday/australia-fires-climate-change.html'
-API_ENDPOINT = "https://api.diffbot.com/v3/article"
-
-ttl = {'token':'3e30c795c4208bafafb20aec78f9ccc1',
-         'url':  url}
-
-r = requests.get(API_ENDPOINT, params = ttl) 
-api_response = r.content
-while Trueimport requests 
-import re
-import json
-
 url = 'https://www.bbc.com/news/world-us-canada-51253066'
 API_ENDPOINT = "https://api.diffbot.com/v3/article"
 
@@ -32,21 +20,6 @@ try:
     author1 = json.loads(api_response)['objects'][0]['author']
 except:
     author1 = 'no author found'
-
-txt = json.loads(api_response)['objects'][0]['text']
-print(title1)
-print(author1)
-print(txt)
-:
-    try:
-        title1 = json.loads(api_response)['objects'][0]['title']
-    except ValueError:
-        title1 = 'no title found'
-while True:
-    try:
-        author1 = json.loads(api_response)['objects'][0]['author']
-    except ValueError:
-        author1 = 'no author found'
 
 txt = json.loads(api_response)['objects'][0]['text']
 print(title1)
