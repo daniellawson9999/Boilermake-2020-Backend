512e05f992d9afa0e2b82b8d1ea6a0bfcb451ebf
Jess Zhang <jesszhang24@gmail.com>
2020-01-26 00:26:59 -0500

    title, author, full text

--- app/request.py
+++ app/request.py
@@ -3,13 +3,17 @@ import re
 import json
 
 url = 'https://nypost.com/2020/01/25/china-steps-up-fight-against-deadly-coronavirus-outbreak/'
-
-param = {'token':'3e30c795c4208bafafb20aec78f9ccc1',
-           'url':  url}
 API_ENDPOINT = "https://api.diffbot.com/v3/article"
-r = requests.get('https://api.diffbot.com/v3/article', params = param) 
-api_response = r.content
 
-formatted = json.loads(api_response)["objects"][0]["html"]
-clean = re.sub(r'<.+?>', '', formatted)
-print(clean)
+ttl = {'token':'3e30c795c4208bafafb20aec78f9ccc1',
+         'url':  url}
+
+r = requests.get(API_ENDPOINT, params = ttl) 
+api_response = r.content
+title1 = json.loads(api_response)["objects"][0]["title"]
+author1 = json.loads(api_response)["objects"][0]["author"]
+txt = json.loads(api_response)["objects"][0]["text"]
+
+print(title1)
+print(author1)
+print(txt)
